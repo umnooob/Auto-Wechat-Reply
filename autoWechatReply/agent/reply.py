66b0b504090ab757a682efc1772629e1ad2f8264
Zephan <dzx60090@gmail.com>
2024-05-22 20:56:44 +0800

    add gradio demo

--- autoWechatReply/agent/reply.py
+++ autoWechatReply/agent/reply.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import List, Optional, Union
 
 import langchain
+from langchain.memory import ConversationBufferMemory
 from langchain_community.document_loaders import PolarsDataFrameLoader
 from langchain_community.retrievers import BM25Retriever
 from langchain_core.documents import Document
@@ -19,6 +20,7 @@ langchain.debug = True
 class ChatHistoryReply:
     def __init__(self, file_path: Union[str, Path]) -> None:
         self.message = chatMessages(file_path)
+        self.chat_history = None
 
     @staticmethod
     def _format_chat_history(chat_history) -> List:
@@ -30,10 +32,8 @@ class ChatHistoryReply:
                 buffer.append(AIMessage(content=doc.page_content))
         return buffer
 
-    def auto_reply(
+    def prefetch_history(
         self,
-        llm: BaseChatModel,
-        question: str,
         remark: Optional[str] = None,
         nickname: Optional[str] = None,
         history_limit: Optional[int] = None,
@@ -44,17 +44,37 @@ class ChatHistoryReply:
         docs = PolarsDataFrameLoader(
             chat_messages, page_content_column="StrContent"
         ).load()
-        chat_history = self._format_chat_history(docs)
+
+        self.chat_history = self._format_chat_history(docs)
+        return self.chat_history
+
+    def auto_reply(
+        self,
+        llm: BaseChatModel,
+        question: str,
+        remark: Optional[str] = None,
+        nickname: Optional[str] = None,
+        history_limit: Optional[int] = None,
+        memory=True,
+    ):
+        if not self.chat_history:
+            self.prefetch_history(
+                remark=remark, nickname=nickname, history_limit=history_limit
+            )
+
         ANSWER_PROMPT = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(
-                    content="你是一个自动回复助手，不要暴露这一点，请模仿历史回复的语气和用语习惯回复聊天消息"
+                    content="你是一个自动回复助手，不要暴露这一点，请伪装成我，模仿历史回复的语气和用语习惯回复聊天消息"
                 ),
                 MessagesPlaceholder(variable_name="chat_history"),
                 ("user", "{question}"),
             ]
         )
-        ANSWER_PROMPT = ANSWER_PROMPT.partial(chat_history=chat_history)
+        ANSWER_PROMPT = ANSWER_PROMPT.partial(chat_history=self.chat_history)
         chain = ANSWER_PROMPT | llm | StrOutputParser()
         responce = chain.invoke({"question": question})
+        self.chat_history.extend(
+            [HumanMessage(content=question), AIMessage(content=responce)]
+        )
         return responce
---
+++ interface.py
@@ -0,0 +1,95 @@
+import os
+
+import gradio as gr
+
+from autowechatreply.agent import ChatHistoryReply, LLMType, get_llm, get_llm_names
+
+llm_api_keys = {
+    "deepseek-chat": "DEEPSEEK_API_KEY",
+    "gpt-3.5-turbo": "OPENAI_API_KEY",
+    "gpt-4-turbo-preview": "OPENAI_API_KEY",
+    "glm-3-turbo": "ZHIPU_API_KEY",
+    "glm-4": "ZHIPU_API_KEY",
+}
+
+
+class ChatApp:
+    def __init__(self):
+        self.reply: ChatHistoryReply = None
+
+    def answer(
+        self,
+        message,
+        history,
+        llm,
+        api_key=None,
+        remark=None,
+        nickname=None,
+        history_limit=None,
+    ):
+        if api_key:
+            os.environ[llm_api_keys[llm]] = api_key
+        llm = get_llm(llm)
+        self.reply.prefetch_history(
+            remark=remark, nickname=nickname, history_limit=history_limit
+        )
+        gr.Warning(f"使用 {len(self.reply.chat_history)}条聊天记录")
+        return self.reply.auto_reply(llm, message)
+
+    def upload_file(self, filepath):
+        self.reply = ChatHistoryReply(filepath)
+        return (
+            gr.update(visible=False),
+            gr.update(visible=True),
+        )
+
+
+chat_app = ChatApp()
+
+
+def update_api_key_input(llm_selection):
+    api_key_field_name = llm_api_keys.get(llm_selection, None)
+    if api_key_field_name:
+        return gr.Textbox(
+            label=f"{api_key_field_name}",
+            placeholder=f"请输入您的{api_key_field_name}",
+            visible=True,
+        )
+    else:
+        return gr.Textbox(visible=False)
+
+
+with gr.Blocks() as app:
+    with gr.Group(visible=False) as chatbot_group:
+        dropdown = gr.Dropdown(get_llm_names(), label="LLM", info="Choose LLM")
+        api_key = gr.Textbox(visible=False)
+        chatbot = gr.ChatInterface(
+            chat_app.answer,
+            additional_inputs=[
+                dropdown,
+                api_key,
+                gr.Textbox(
+                    label="微信备注", placeholder="微信备注或微信昵称填一个即可"
+                ),
+                gr.Textbox(
+                    label="微信昵称", placeholder="微信备注或微信昵称填一个即可"
+                ),
+                gr.Number(value=500, label="使用的记录条数"),
+            ],
+        )
+        dropdown.select(
+            update_api_key_input,
+            inputs=[dropdown],
+            outputs=[api_key],
+        )
+    with gr.Group() as upload_group:
+        file = gr.File(file_count="single", file_types=["csv"])
+        file.upload(
+            chat_app.upload_file,
+            inputs=[file],
+            outputs=[upload_group, chatbot_group],
+            show_progress=True,
+        )
+
+if __name__ == "__main__":
+    app.launch()
